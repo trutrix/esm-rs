aecf558a2b88eb3af2ee99726032674508fc97fe
Colton Chaffos <trutrix@gmail.com>
2024-10-04 15:30:12 -0600

    Added FileEntry and TextureEntry

--- bindings/ba2.h
+++ bindings/ba2.h
@@ -8,3 +8,30 @@ struct BA2Header {
     uint32_t file_count;
     uint64_t name_table_offset;
 };
+
+struct FileEntry {
+    uint32_t unknown0;
+    typeid_t ext;
+    uint32_t unknown1;
+    uint32_t unknown2;
+    uint64_t offset;
+    uint32_t packed_size;
+    uint32_t unpacked_size;
+    uint32_t unknown3;
+};
+
+
+struct TextureEntry {
+    uint32_t filename_hash;
+    uint32_t file_extension;
+    uint32_t directory_hash;
+    uint8_t unknown;
+    uint8_t num_chunks;
+    uint16_t chunk_header_size;
+    uint16_t texture_height;
+    uint16_t texture_width;
+    uint8_t mip_levels;
+    uint8_t format;
+    uint8_t is_cubemap;
+    uint8_t tile_mode;
+};
