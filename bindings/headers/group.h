ab1d0e715486b6a9b9f76c99ae72543bd5aaed4a
trutrix <trutrix@gmail.com>
2024-10-11 12:53:09 -0600

    Removed Group enum, does not work good with bindgen

--- bindings/headers/group.h
+++ bindings/headers/group.h
@@ -1,28 +1,24 @@
 
 #include "../types.h"
 
-enum GroupType: uint32_t {
-    Top = 0,
-    WorldChildren = 1,
-    InteriorCellBlock = 2,
-    InteriorCellSubBlock = 3,
-    ExteriorCellBlock = 4,
-    ExteriorCellSubBlock = 5,
-    CellChildren = 6,
-    TopicChildren = 7,
-    CellPersistantChildren = 8,
-    CellTemporaryChildren = 9,
-    CellVisibleDistantChildren = 10
-};
-
 struct GroupHeader {
     typeid_t type_id;
     uint32_t size;
     typeid_t label;
-    enum GroupType type;
+    uint32_t type;
     timestamp16_t timestamp;
     version16_t version;
     uint32_t unknown;
 };
 
-
+const uint8_t TOP_GROUP = 0;
+const uint8_t WORLD_CHILDREN = 1;
+const uint8_t EXTERIOR_CELL_BLOCK = 2;
+const uint8_t EXTERIOR_CELL_SUB_BLOCK = 3;
+const uint8_t INTERIOR_CELL_BLOCK = 4;
+const uint8_t INTERIOR_CELL_SUB_BLOCK = 5;
+const uint8_t CELL_CHILDREN = 6;
+const uint8_t TOPIC_CHILDREN = 7;
+const uint8_t CELL_PERSISTANT_CHILDREN = 8;
+const uint8_t CELL_TEMPORARY_CHILDREN = 9;
+const uint8_t CELL_VISIBLE_DISTANT_CHILDREN = 10;
