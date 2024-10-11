23d1eca7d840ea2e12818b3f53034c9b8a56d1e1
trutrix <trutrix@gmail.com>
2024-10-11 10:57:23 -0600

    Change group type to enum

--- bindings/headers/group.h
+++ bindings/headers/group.h
@@ -1,12 +1,28 @@
 
 #include "../types.h"
 
+enum group_label_t {
+    Top = 0,
+    WorldChildren = 1,
+    InteriorCellBlock = 2,
+    InteriorCellSubBlock = 3,
+    ExteriorCellBlock = 4,
+    ExteriorCellSubBlock = 5,
+    CellChildren = 6,
+    TopicChildren = 7,
+    CellPersistantChildren = 8,
+    CellTemporaryChildren = 9,
+    CellVisibleDistantChildren = 10
+};
+
 struct GroupHeader {
     typeid_t type_id;
     uint32_t size;
     typeid_t label;
-    int32_t type;
+    enum group_label_t type;
     timestamp16_t timestamp;
     version16_t version;
     uint32_t unknown;
 };
+
+
