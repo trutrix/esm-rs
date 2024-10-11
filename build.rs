24498aecb60c74307024ee09d5a1482bb949f10b
trutrix <trutrix@gmail.com>
2024-10-11 12:41:46 -0600

    Reverted enum generation

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 fn generate_bindings(h: &str, rs: &str) {
     let bindings = bindgen::Builder::default()
         .header(h)
-        .default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: false })
+        //.default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: false })
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new())) // track header changes with cargo
         .generate()
         .expect("Unable to generate bindings");
