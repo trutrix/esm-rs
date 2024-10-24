75c4afa3568165b5ca16cea0bfaf5d93d442f4ed
trutrix <trutrix@gmail.com>
2024-10-24 08:32:29 -0600

    Made RecordHeader debug display chars if the type_id is GRUP

--- src/fo3/headers.rs
+++ src/fo3/headers.rs
@@ -5,7 +5,11 @@ use super::RecordHeader;
 
 impl std::fmt::Debug for RecordHeader {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.type_id == b"GRUP" {
+            write!(f, "Record {{ Type: {:?}, Size, {:?}, Flags as FourCC: {:?}, Id: {:?} }}", {self.type_id}, {self.size}, {fourcc::FourCC(self.flags.to_le_bytes())}, {self.id})
+        } else {
         
-        write!(f, "Record {{ Type: {:?}, Size, {:?}, Flags: {:?}, Id: {:?} }}", {self.type_id}, {self.size}, {self.flags}, {self.id})
+            write!(f, "Record {{ Type: {:?}, Size, {:?}, Flags: {:?}, Id: {:?} }}", {self.type_id}, {self.size}, {self.flags}, {self.id})
+        }
     }
 }
